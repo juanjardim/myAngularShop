09e46ec163e3572b23cfaafb16ce0024d61475e4
juan jardim <juanjardim@gmail.com>
2017-09-17 15:20:59 +0200

    Create the base for the application

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDfeMO3O3WYzRTnjbRg8Rtvovc59wjVPZI",
+    authDomain: "myshop-59c7c.firebaseapp.com",
+    databaseURL: "https://myshop-59c7c.firebaseio.com",
+    projectId: "myshop-59c7c",
+    storageBucket: "",
+    messagingSenderId: "580828492922"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDfeMO3O3WYzRTnjbRg8Rtvovc59wjVPZI",
+    authDomain: "myshop-59c7c.firebaseapp.com",
+    databaseURL: "https://myshop-59c7c.firebaseio.com",
+    projectId: "myshop-59c7c",
+    storageBucket: "",
+    messagingSenderId: "580828492922"
+  }
 };
