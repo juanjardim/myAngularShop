ca447b4ec9512b603dc31598eb69643e73e6f3e8
juan jardim <juanjardim@gmail.com>
2017-09-18 08:58:38 +0200

    Implementing authorization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AdminAuthGuard } from './admin-auth-guard/admin-auth-guard.service';
+import { UserService } from './user/user.service';
 import { AuthGuard } from './auth-guard/auth-guard.service';
 import { AuthService } from './auth/auth.service';
 import { environment } from './../environments/environment';
@@ -53,13 +55,15 @@ import { LoginComponent } from './login/login.component';
       { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
       { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
       
-      { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard]  },
-      { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard]  },
+      { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard, AdminAuthGuard]  },
+      { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard, AdminAuthGuard]  },
     ])
   ],
   providers: [
     AuthService,
-    AuthGuard
+    AuthGuard,
+    UserService,
+    AdminAuthGuard
   ],
   bootstrap: [AppComponent]
 })
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,25 +1,38 @@
+import { UserService } from './../user/user.service';
+import { AppUser } from './../models/app-user';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
 import * as firebase from 'firebase';
 
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
+
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(private userService: UserService, private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
   login() {
     var returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
-    
+
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .switchMap(user => {
+        if (user) return this.userService.get(user.uid);
+        return Observable.of(null);
+      });
   }
 }
